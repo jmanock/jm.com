c23c8f3a2791c242c1fb6a3202a8ab91f132c20c
Jon Manock <jonmanock@Jons-MacBook-Pro.local>
2019-03-05 11:33:17 -0500

    rebuilding cal

---
+++ src/Components/uploader.jsx
@@ -0,0 +1,48 @@
+import React, {Component} from 'react';
+import {storage} from './firebase';
+
+class Uploader extends Component{
+  state = {
+    image:null,
+    url:'',
+    progress:0
+  }
+
+  handleChange = e =>{
+    if(e.target.files[0]){
+      const image = e.target.files[0];
+      this.setState(() => ({image}));
+    }
+  }
+
+  handleUpload = () =>{
+    const {image} = this.state;
+    const uploadTask = storage.ref(`images/${image.name}`).put(image);
+    uploadTask.on('state_changed', (snapshot) =>{
+      const progress = Math.round((snapshot.bytesTransferred / snapshot.totalBytes) *100);
+      this.setState({progress})
+    }, (error) => {
+      console.log(error);
+    }, () =>{
+      storage.ref('images').child(image.name).getDownloadURL().then(url => {
+        console.log(url);
+        this.setState({url})
+      })
+    });
+  }
+
+  render(){
+    return(
+      <div>
+        <progress value={this.state.progress} max='100' />
+        <br />
+        <input type='file' onChange={this.handleChange}></input>
+        <button onClick={this.handleUpload}>Upload</button>
+        <br />
+        <img src={this.state.url || 'http://via.placeholder.com/400x300'} alt='Upload Image'></img>
+      </div>
+    )
+  }
+}
+
+export default Uploader;
